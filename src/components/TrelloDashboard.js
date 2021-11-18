8097b8a39d7fd0b2ffe60e36040d1ba35a8b5386
devbewill <sedprc@gmail.com>
2021-11-18 14:27:56 +0100

    refactor trello without trello-web

--- src/components/TrelloDashboard.js
+++ src/components/TrelloDashboard.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export const TrelloDashboard = () => {
-  // const TRELLO_TOKEN =
-  //   "262f10ddc7962d28d4b472bc17c46e39c069a401a54abc269014c209f378f83e";
+  const TRELLO_TOKEN ="262f10ddc7962d28d4b472bc17c46e39c069a401a54abc269014c209f378f83e";
   const TRELLO_KEY = "42cd6a54f624ac7948e46f5198e4a94b";
-  const Trello = require("trello-web");
-  const trello = new Trello(TRELLO_KEY);
+  const TRELLO_BOARD = "0retKS6l"
+  const baseURL = `https://api.trello.com/1/boards/${TRELLO_BOARD}/cards?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`
+
+  let listName;
+ 
   const [lists, setLists] = useState([]);
   const [cards, setCards] = useState([]);
-
   const groupByKey = (array, key) => {
     return array.reduce((hash, obj) => {
       if (obj[key] === undefined) return hash;
@@ -17,54 +19,21 @@ export const TrelloDashboard = () => {
       });
     }, {});
   };
+  let splitByCard = groupByKey(cards, "idList");
 
   useEffect(() => {
-    Promise.resolve()
-      .then(() => localStorage.getItem("token"))
-      .then((existingToken) => {
-        if (existingToken) {
-          trello.token = existingToken;
-        } else {
-          return trello.auth({
-            name: "My Trello App",
-            scope: {
-              read: true,
-              write: false,
-              account: true,
-            },
-            expiration: "1hour",
-          });
-        }
-      })
-      .then(() =>
-        // trello.get("/1/tokens/" + trello.token + "/member", {
-        //   fields: 'username,id,email'
-        // })
-        // trello.get('/1/members/me/boards').then(console.log)
-        trello.get("/1/boards/0retKS6l/lists").then((data) => {
-          setLists(data);
-        })
-      )
-      .then(() =>
-        trello.get("/1/boards/0retKS6l/cards").then((data) => {
-          setCards(data);
-        })
-      )
-      .catch((e) => {
-        console.log(
-          "something bad happened, or the user took too long to authorize.",
-          e
-        );
-      });
-  }, [ setCards, setLists]);
 
-  let splitByCard = groupByKey(cards, "idList");
-  let listName;
+    axios.get(baseURL).then((response) => {
+      // setPost(response.data);
+      setCards(response.data)
+
+      });
+  }, []);
+
 
   return (
     <div className="trelloDash">
       {Object.entries(splitByCard).map((list, i) => {
-
         if (list[0] === "606f1a2bebb5fb53804dd3d5") {
           listName = "TO DO";
         } else if (list[0] === "606f1a4ed2ff554e4ea11b82") {
